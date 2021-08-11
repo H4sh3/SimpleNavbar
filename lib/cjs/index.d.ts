2d007e8df093a959ba6c2afd5246a72850ce76b6
daniel <pinkdaniel@web.de>
2021-08-11 20:43:18 +0200

    enhance

--- lib/cjs/index.d.ts
+++ lib/cjs/index.d.ts
@@ -2,6 +2,8 @@ import React from "react";
 export interface SimpleNavbarProps {
     links: React.ReactElement[];
     logo: React.ReactElement;
+    maxMenuVh: string;
+    bgColorOnHover: string;
 }
 declare const SimpleNavbar: React.FC<SimpleNavbarProps>;
 export default SimpleNavbar;
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 
 export interface SimpleNavbarProps {
     links: React.ReactElement[],
-    logo: React.ReactElement
+    logo: React.ReactElement,
+    maxMenuVh: string,
+    bgColorOnHover: string
 }
 
-const SimpleNavbar: React.FC<SimpleNavbarProps> = ({ children, links, logo }) => {
+const SimpleNavbar: React.FC<SimpleNavbarProps> = ({ children, links, logo, maxMenuVh, bgColorOnHover = 'bg-black' }) => {
     const [menuOpen, setMenuOpen] = useState(false)
 
     const [shrink, setShrink] = useState(false)
@@ -30,11 +32,17 @@ const SimpleNavbar: React.FC<SimpleNavbarProps> = ({ children, links, logo }) =>
     return (
         <div>
             <div id="header" style={{ "transition": "all .4s ease-in-out", "height": navbarHeight }}
-                className="z-10 fixed top-0 left-0 w-full text-white hover:bg-black navbarGradient">
-                <div className="h-full flex justify-between gap-4">
-                    {logo}
+                className={`z-10 fixed top-0 left-0 w-full text-white hover:${bgColorOnHover} navbarGradient`}>
+                <div className="h-full flex justify-between items-center gap-4">
+                    <div>
+                        {logo}
+                    </div>
                     <nav className="hidden lg:flex lg:flex-row lg:items-center gap-4 text-xl">
-                        {links}
+                        {links.map((e, i) => {
+                            return <React.Fragment key={i}>
+                                {e}
+                            </React.Fragment>
+                        })}
                     </nav>
                     <div className="lg:hidden self-center">
                         <button onClick={() => { setMenuOpen(!menuOpen) }} id="nav-toggle" className="flex items-center p-1 text-red-500 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
@@ -45,16 +53,20 @@ const SimpleNavbar: React.FC<SimpleNavbarProps> = ({ children, links, logo }) =>
                     </div>
                 </div>
             </div>
-            <div style={{ "height": "8vh" }}></div>
-            <div className="fixed w-full bg-gray-100" style={{ opacity: menuOpen ? 1 : 0, "height": menuOpen ? '30vh' : '0vh', "transition": "all 0.4s ease-in-out" }}>
+            <div style={{ "height": "6vh" }}></div>
+            <div className="z-9 fixed w-full bg-gray-100" style={{ opacity: menuOpen ? 1 : 0, "height": menuOpen ? maxMenuVh : '0vh', "transition": "all 0.4s ease-in-out" }}>
                 {
                     menuOpen ?
                         <div style={{ "height": menuOpen ? '90vh' : '0vh', "transition": "all 0.4s ease-in-out" }}>
-                            <div style={{ "height": "12vh" }}></div>
-                            <div className="divide-y-2 divide-red-400 w-full grid grid-cols-1 justify-items-center">
-                                {links}
+                            <div style={{ "height": "7vh" }}></div>
+                            <div className="gap-2 w-full grid grid-cols-1 justify-items-center">
+                                {links.map((e, i) => {
+                                    return <React.Fragment key={i}>
+                                        {e}
+                                    </React.Fragment>
+                                })}
                             </div>
-                            <div className="pt-10 w-full text-center" onClick={() => { setMenuOpen(false) }}>
+                            <div className="pr-2 pt-10 w-full text-center" onClick={() => { setMenuOpen(false) }}>
                                 &#9650;
                             </div>
                         </div>
