fb24981a50d7ac8b96e631bf8bba899f0c9ae1de
daniel <pinkdaniel@web.de>
2021-08-12 10:15:08 +0200

    minor changes

--- src/index.tsx
+++ src/index.tsx
@@ -37,14 +37,14 @@ const SimpleNavbar: React.FC<SimpleNavbarProps> = ({ children, links, logo, maxM
                     <div>
                         {logo}
                     </div>
-                    <nav className="hidden lg:flex lg:flex-row lg:items-center gap-4 text-xl">
+                    <nav className="hidden lg:flex lg:flex-row lg:items-center gap-4 text-xl mr-5">
                         {links.map((e, i) => {
                             return <React.Fragment key={i}>
                                 {e}
                             </React.Fragment>
                         })}
                     </nav>
-                    <div className="lg:hidden self-center">
+                    <div className="lg:hidden self-center pr-4">
                         <button onClick={() => { setMenuOpen(!menuOpen) }} id="nav-toggle" className="flex items-center p-1 text-red-500 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
                             <svg className="fill-current h-6 w-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
@@ -54,7 +54,7 @@ const SimpleNavbar: React.FC<SimpleNavbarProps> = ({ children, links, logo, maxM
                 </div>
             </div>
             <div style={{ "height": "6vh" }}></div>
-            <div className="z-9 fixed w-full bg-gray-100" style={{ opacity: menuOpen ? 1 : 0, "height": menuOpen ? maxMenuVh : '0vh', "transition": "all 0.4s ease-in-out" }}>
+            <div className="lg:hidden z-9 fixed w-full bg-gray-100" style={{ opacity: menuOpen ? 1 : 0, "height": menuOpen ? maxMenuVh : '0vh', "transition": "all 0.4s ease-in-out" }}>
                 {
                     menuOpen ?
                         <div style={{ "height": menuOpen ? '90vh' : '0vh', "transition": "all 0.4s ease-in-out" }}>
@@ -66,8 +66,10 @@ const SimpleNavbar: React.FC<SimpleNavbarProps> = ({ children, links, logo, maxM
                                     </React.Fragment>
                                 })}
                             </div>
-                            <div className="pr-2 pt-10 w-full text-center" onClick={() => { setMenuOpen(false) }}>
-                                &#9650;
+                            <div className="pt-10 w-full flex flex-row justify-center">
+                                <div className="p-5 cursor-pointer text-center" onClick={() => { setMenuOpen(false) }}>
+                                    &#9650;
+                                </div>
                             </div>
                         </div>
                         : <></>
